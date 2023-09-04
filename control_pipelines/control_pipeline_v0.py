flat system fixed, add scale to normalization, save waypoint trajectories, remove sample weights, updated V_safe

control_pipelines/control_pipeline_v0.py
@@ -8,6 +8,7 @@ from optCtrl.lqr import LQRSolver
 from trajectory.trajectory import Trajectory, SystemConfig
 from control_pipelines.base import ControlPipelineBase
 from control_pipelines.control_pipeline_v0_helper import ControlPipelineV0Helper
+from trajectory.spline.spline_3rd_order import vehicle_flat_forward, vehicle_flat_reverse
 import math
 
 
@@ -19,47 +20,47 @@ import control.optimal as opt
 from scipy.optimize import LinearConstraint
 
 
-# Function to take states, inputs and return the flat flag
-def vehicle_flat_forward(x, u, params={}):
-    # Get the parameter values
-    # Create a list of arrays to store the flat output and its derivatives
-    zflag = [np.zeros(4), np.zeros(4)]
-    # Flat output is the x, y position of the rear wheels
-    zflag[0][0] = x[0]
-    zflag[1][0] = x[1]
-    theta = x[2]
-    vel = x[3]
-    # zflag[3][0] = x[3]
-    # First derivatives of the flat output
-    zflag[0][1] = vel * np.cos(theta)  # dx/dt
-    zflag[1][1] = vel * np.sin(theta)  # dy/dt
-    # zflag[2][1] = u[0]
-    # zflag[3][1] = u[1]
-    # First derivative of the angle
-    thdot = u[0]
-    vdot = u[1]
-    # Second derivatives of the flat output (setting vdot = 0)
-    zflag[0][2] = - vel * thdot * np.sin(theta) + vdot * np.cos(theta)
-    zflag[1][2] = vel * thdot * np.cos(theta) + vdot * np.sin(theta)
-    return zflag
-
-
-# Function to take the flat flag and return states, inputs
-def vehicle_flat_reverse(zflag, params={}):
-    # Get the parameter values
-    # Create a vector to store the state and inputs
-    x = np.zeros(4)
-    u = np.zeros(2)
-    # Given the flat variables, solve for the state
-    x[0] = zflag[0][0]  # x position
-    x[1] = zflag[1][0]  # y position
-    x[2] = np.arctan2(zflag[1][1], zflag[0][1])  # tan(theta) = ydot/xdot
-    x[3] = np.linalg.norm([zflag[1][1], zflag[0][1]])
-    # And next solve for the inputs
-    u[0] = 1 / (1 + (zflag[0][1] / zflag[0][1]) ** 2) * (
-            (zflag[1][2] * zflag[0][1]) - (zflag[0][2] * zflag[1][1])) / (zflag[0][1] ** 2+1e-5)
-    u[1] = 0.5 * (1 / x[3]) * (2 * zflag[1][2] * zflag[1][1] + 2 * zflag[0][2] * zflag[0][1])
-    return x, u
+# # Function to take states, inputs and return the flat flag
+# def vehicle_flat_forward(x, u, params={}):
+#     # Get the parameter values
+#     # Create a list of arrays to store the flat output and its derivatives
+#     zflag = [np.zeros(4), np.zeros(4)]
+#     # Flat output is the x, y position of the rear wheels
+#     zflag[0][0] = x[0]
+#     zflag[1][0] = x[1]
+#     theta = x[2]
+#     vel = x[3]
+#     # zflag[3][0] = x[3]
+#     # First derivatives of the flat output
+#     zflag[0][1] = vel * np.cos(theta)  # dx/dt
+#     zflag[1][1] = vel * np.sin(theta)  # dy/dt
+#     # zflag[2][1] = u[0]
+#     # zflag[3][1] = u[1]
+#     # First derivative of the angle
+#     thdot = u[1]
+#     vdot = u[0]
+#     # Second derivatives of the flat output (setting vdot = 0)
+#     zflag[0][2] = - vel * thdot * np.sin(theta) + vdot * np.cos(theta)
+#     zflag[1][2] = vel * thdot * np.cos(theta) + vdot * np.sin(theta)
+#     return zflag
+#
+#
+# # Function to take the flat flag and return states, inputs
+# def vehicle_flat_reverse(zflag, params={}):
+#     # Get the parameter values
+#     # Create a vector to store the state and inputs
+#     x = np.zeros(4)
+#     u = np.zeros(2)
+#     # Given the flat variables, solve for the state
+#     x[0] = zflag[0][0]  # x position
+#     x[1] = zflag[1][0]  # y position
+#     x[2] = np.arctan2(zflag[1][1], zflag[0][1])  # tan(theta) = ydot/xdot
+#     x[3] = np.linalg.norm([zflag[1][1], zflag[0][1]])
+#     # And next solve for the inputs
+#     u[0] = 1 / (1 + (zflag[0][1] / zflag[0][1]) ** 2) * (
+#             (zflag[1][2] * zflag[0][1]) - (zflag[0][2] * zflag[1][1])) / (zflag[0][1] ** 2+1e-5)
+#     u[1] = 0.5 * (1 / x[3]) * (2 * zflag[1][2] * zflag[1][1] + 2 * zflag[0][2] * zflag[0][1])
+#     return x, u
 
 
 
@@ -274,9 +275,12 @@ class ControlPipelineV0(ControlPipelineBase):
         import tqdm
         for idx, xf in tqdm.tqdm(enumerate(xfs), total=xfs.shape[0]):
             try:
-                traj_const = fs.point_to_point(vehicle_flat, t, x0, u0, xf, uf, basis=None, # fs.PolyFamily(8),
-                                               constraints=trajectory_constraints)
-            except RuntimeError:
+                traj_const = fs.point_to_point(
+                    vehicle_flat, t, x0, u0, xf, uf, basis=None, # fs.PolyFamily(18),
+                    constraints=trajectory_constraints
+               )
+            except RuntimeError as e:
+                print(e)
                 continue
             # ,cost=cost_fcn)
             # Create the trajectory
@@ -565,7 +569,10 @@ class ControlPipelineV0(ControlPipelineBase):
         FRS = np.load(
             '/local-scratch/tara/project/WayPtNav-reachability/optimized_dp-master/FRS_result3/FRS_v{:.2f}_H6.npy'.format(
                 v0))
-        result = np.where(FRS <= 0)
+        if v0<0.05:
+            result = np.where(FRS <= 0)
+        else:
+            result = np.where(FRS[:,:,:,:-5] <= 0)
         n = len(result[0])
         wx_n_all = 0 + result[0] * 5 / 100
         wy_n_all = -5 + result[1] * 10 / 100

models/base.py
@@ -34,7 +34,8 @@ from scipy.interpolate import interp1d
 from sklearn import metrics
 from sbpd.sbpd_renderer import SBPDRenderer
 from itertools import combinations_with_replacement as combinations_w_r
-
+from time import time
+from systems.dubins_car import DubinsCar
 
 class PolynomialFeaturesLayer(tf.keras.layers.Layer):
     def __init__(self, degree):
@@ -470,11 +471,17 @@ class BaseModel(object):
             sample = 1  #600 , 50
 
             biases = nn_output[:, :4]
+            scales = nn_output[:, 4:8]
+            kernel_weights = nn_output[:, 8:]
 
-            def normalize(X, bias):
-                return tf.convert_to_tensor(X.astype(np.float32)) + bias
+            def normalize(X, biases, scales):
+                if isinstance(X, np.ndarray):
+                    X = tf.convert_to_tensor(X)
+                # expand for all waypoints
+                return tf.expand_dims(scales, 1) * (tf.cast(X, dtype=tf.float32) + tf.expand_dims(biases, 1))
 
-            X_norm = [normalize(X, bias) for X, bias in zip(feat_train_sc, biases) ]
+            # X_norm = [normalize(X, bias, scale) for X, bias, scale in zip(feat_train_sc, biases, scales) ]
+            X_norm = normalize(feat_train_sc, biases, scales)
 
             # poly = PolynomialFeatures(3)
             # X_kerneled = [  poly.fit_transform(X).astype(np.float32) for X in X_norm ]
@@ -484,7 +491,7 @@ class BaseModel(object):
                 self.polyfeatures(X) for X in X_norm
             ]
             X_kerneled = tf.stack(X_kerneled, axis=0)
-
+            #
             # X_kerneled = [  poly.fit_transform(X).astype(np.float32) for X in X_norm ]
 
             # X_kerneled = tf.stack([self.poly_layer(X) for X in X_norm], axis=0)
@@ -517,7 +524,7 @@ class BaseModel(object):
 
             predicted = [K.dot(tf.convert_to_tensor(x1, dtype=tf.float32), tf.expand_dims(output, axis=1)) for
                          x1, output in
-                         zip(X_kerneled, nn_output[:, 4:])]
+                         zip(X_kerneled, kernel_weights)]
 
             # X[:, 0] =
             # x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
@@ -528,7 +535,7 @@ class BaseModel(object):
 
             # predicted_contour = [K.dot(tf.convert_to_tensor(x1, dtype=tf.float32), tf.expand_dims(output, axis=1)) for
             #              x1, output in
-            #              zip(X_kerneled, nn_output[:, 4:])]
+            #              zip(X_kerneled, kernel_weights)]
 
 
 
@@ -546,14 +553,14 @@ class BaseModel(object):
                 n_sample1 = np.size(np.where(label == -1)[0])
                 # sample_weights  = (11 / 9 + label) * 9 / 2
                 # weight= weightb (weightS+ytrue)
-                if (n_sample1 != 0 and n_sample0 != 0):
-                    # r = math.sqrt(n_sample0 / n_sample1)
-                    r = n_sample0 / n_sample1
-                    weightS = (r + 1) / (r - 1)
-                    weightb =  1 / (weightS - 1)
-                    sample_weights = (weightS - label) * weightb
-                else:
-                    sample_weights = tf.ones(label.shape)
+                # if (n_sample1 != 0 and n_sample0 != 0):
+                #     # r = math.sqrt(n_sample0 / n_sample1)
+                #     r = n_sample0 / n_sample1
+                #     weightS = (r + 1) / (r - 1)
+                #     weightb =  1 / (weightS - 1)
+                #     sample_weights = (weightS - label) * weightb
+                # else:
+                sample_weights = tf.ones(label.shape)
                 # sample_weights = np.array([n_sample0 / n_sample1 if i == 1 else 1.0 for i in label])
                 # class_weights[np.where(label == 1)[0]] = 1.0 / n_sample1
                 # class_weights[np.where(label == -1)[0]] = 1.0 / n_sample0
@@ -623,32 +630,45 @@ class BaseModel(object):
             renderer = SBPDRenderer.get_renderer(self.p.simulator_params.obstacle_map_params.renderer_params)
 
             # 2d plots
-            pdf = PdfPages("output_fov_sample40_FRS_4.pdf")
-            for WP, prediction, label, C1, image, start_nk3, goal, wp, speed, robot_pos,robot_head in zip(
+            stamp = time()
+            pdf = PdfPages(f"output_fov_sample40_FRS_4_{stamp:.2f}.pdf")
+            for img_idx, (WP, prediction, label, C1, image, start_nk3, goal, traj, wp, speed, robot_pos,robot_head) in enumerate(zip(
                     processed_data['Action_waypoint'], prediction_total, processed_data['labels'],
                     nn_output.numpy(),
                     raw_data['img_nmkd'][:, :, :, :3],
                     raw_data['start_state'],
                     raw_data['goal_position_n2'],
+                    raw_data['vehicle_state_nk3'],
                     all_waypoint_sampled,
-                    processed_data['inputs'][1], camera_grid_world_pos_12, camera_pos_13):#, predicted_contour):
+                    processed_data['inputs'][1], camera_grid_world_pos_12, camera_pos_13)):#, predicted_contour):
 
                 label = -label
                 # robot
                 crop_size = [100, 100]
                 top = renderer._get_topview(robot_pos, robot_head, crop_size)
                 # ax = plt.figure()
-                fig, ax = plt.subplots()
-                ax.imshow(np.squeeze(top))
-                ax.plot(0, (crop_size[0] - 1) / 2, 'k*')
+                fig = plt.figure()
+                ax4 = fig.add_subplot(224)
+                # fig, ax = plt.subplots()
+                ax4.imshow(np.squeeze(top))
+                ax4.plot(0, (crop_size[0] - 1) / 2, 'k*')
                 color = ['red' if l == 1 else 'green' for l in label]
                 WP_map_x = (WP[:, 0]/dx + 0)
                 WP_map_y = (WP[:, 1]/dx + (crop_size[0] - 1) / 2)
                 plt.scatter(WP_map_x, WP_map_y, marker= 'o', color=color,  s=5)
+
+                traj_ego = DubinsCar.convert_position_and_heading_to_ego_coordinates(
+                    np.expand_dims(start_nk3, 0),
+                    np.expand_dims(traj[:, :3], 0)
+                )
+                traj_x = (traj_ego[:, 0] / dx + 0)
+                traj_y = (traj_ego[:, 1] / dx + (crop_size[0] - 1) / 2)
+                plt.plot(traj_x, traj_y)
+
                 theta = np.pi / 2 + WP[:, 2:3]  # theta of the arrow
                 u, v = 1 * (np.cos(theta), np.sin(theta))
                 q = plt.quiver(WP_map_x, WP_map_y, u, v)
-                ax.set_title('speed ' + str(speed))
+                ax4.set_title('speed ' + str(speed))
 
                 x_min, x_max = 0, crop_size[0]*dx
                 y_min, y_max = -dx* (crop_size[0] - 1)/2 , dx* (crop_size[0] -1)/2
@@ -657,9 +677,17 @@ class BaseModel(object):
                                      np.arange(y_min, y_max, h))
                 hh, ss = np.tile(0, np.shape(xx)), np.tile(speed, np.shape(xx)),
                 X_grid = np.c_[xx.ravel(), yy.ravel(), hh.ravel(), ss.ravel()]
-                X_grid = tf.expand_dims(X_grid,axis=0)
+                X_grid = tf.expand_dims(X_grid, axis=0)
+                # X_grid = [normalize(X, bias) for X, bias in zip(X_grid, biases)]
+                X_grid = normalize(
+                    X_grid,
+                    biases[img_idx:(img_idx+1)],
+                    scales[img_idx:(img_idx+1)]
+                )
+
+                # X_grid = tf.expand_dims(X_grid,axis=0)
                 # X_grid_kerneled = [poly.fit_transform(X).astype(np.float32) for X in X_grid]
-                # X_grid_kerneled = np.array(X_kerneled)
+                # X_grid_kerneled = np.array(X_grid_kerneled)
                 X_grid_kerneled = [
                     self.polyfeatures(tf.constant(X)) for X in X_grid
                 ]
@@ -676,9 +704,9 @@ class BaseModel(object):
                 # # X_grid_kerneled = tf.stack([self.poly_layer(tf.cast(X, tf.float32)) for X in X_grid], axis=0)
                 Z = [-np.sign(K.dot(tf.cast(x1, tf.float32), tf.expand_dims(output, axis=1))) for
                      x1, output in
-                     zip(X_grid_kerneled, nn_output[:, 4:])]
+                     zip(X_grid_kerneled, kernel_weights)]
                 Z = np.array(Z)
-                ax.contourf(xx/dx+0, yy/dx+(crop_size[0] - 1) / 2, np.reshape(np.squeeze(Z), np.shape(xx)), cmap=plt.get_cmap("RdBu"),  alpha=0.5)
+                ax4.contourf(xx/dx+0, yy/dx+(crop_size[0] - 1) / 2, np.reshape(np.squeeze(Z), np.shape(xx)), cmap=plt.get_cmap("RdBu"),  alpha=0.5)
                 # plt.show()
                 #
                 # x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
@@ -687,7 +715,7 @@ class BaseModel(object):
                 #                      np.arange(y_min, y_max, h))
 
 
-                fig = plt.figure()
+                # fig = plt.figure()
                 ax1 = fig.add_subplot(221)
                 ax1.imshow(image.astype(np.uint8))
                 plt.grid()
@@ -782,22 +810,22 @@ class BaseModel(object):
                 # u, v =  1 * (np.cos(theta_world), np.sin(theta_world))
                 # q = ax3.quiver(pos_nk2[:, 0], pos_nk2[:, 1], u, v)
                 # plt.show()
-                ax4 = fig.add_subplot(224)
-                x = WP[:, 0]
-                y = WP[:, 1]
-                theta = WP[:, 2]
-                u, v = 1 * (np.cos(theta), np.sin(theta))
-                accuracy = np.count_nonzero(prediction == label) / np.size(label)
-                color_result = ['red' if l == -1 else 'green' for l in prediction]
-                wrong = WP[np.where(prediction != label)[0]]
-                ax4.scatter(wrong[:, 0], wrong[:, 1], s=60, edgecolors="k")
-                ax4.scatter(x, y
-                            , marker='o', alpha=0.6, color=color_result)
-                # ax2.scatter3D(wrong[:, 0], wrong[:, 1],
-                #               wrong[:, 2], s=80, edgecolors="k")
-                # safe = WP[np.where(prediction == 1)[0]]
-                # unsafe = WP[np.where(prediction == -1)[0]]
-                ax4.set_title('accuracy: ' + str(accuracy))
+                # ax4 = fig.add_subplot(224)
+                # x = WP[:, 0]
+                # y = WP[:, 1]
+                # theta = WP[:, 2]
+                # u, v = 1 * (np.cos(theta), np.sin(theta))
+                # accuracy = np.count_nonzero(prediction == label) / np.size(label)
+                # color_result = ['red' if l == -1 else 'green' for l in prediction]
+                # wrong = WP[np.where(prediction != label)[0]]
+                # ax4.scatter(wrong[:, 0], wrong[:, 1], s=60, edgecolors="k")
+                # ax4.scatter(x, y
+                #             , marker='o', alpha=0.6, color=color_result)
+                # # ax2.scatter3D(wrong[:, 0], wrong[:, 1],
+                # #               wrong[:, 2], s=80, edgecolors="k")
+                # # safe = WP[np.where(prediction == 1)[0]]
+                # # unsafe = WP[np.where(prediction == -1)[0]]
+                # ax4.set_title('accuracy: ' + str(accuracy))
                 # ax4.scatter(WP[:, 0], WP[:, 1]
                 #             , c=np.squeeze(prediction), marker='o', alpha=0.6, cmap=mycmap)
                 q1 = ax4.quiver(x, y, u, v)
@@ -857,7 +885,7 @@ class BaseModel(object):
             # prediction_loss = tf.zeros(1)
             # num_total = 0
             # # for (y, x, w, weights_v) in zip(processed_data['labels'],  X_kerneled, nn_output, 1 - processed_data['inputs'][1][:, 0]):
-            # for y, x, w in zip(processed_data['labels'], X_kerneled, nn_output[:, 4:]):
+            # for y, x, w in zip(processed_data['labels'], X_kerneled, kernel_weights):
             #     # weights_v = 1 - processed_data['inputs'][1][:, 0]
             #     for x1 , y1 in zip(x , y):
             #         v = y1 * tf.tensordot(w, x1, axes=1)

objectives/objective_function.py
@@ -102,7 +102,7 @@ class ObjectiveFunction(object):
                 if tag == 'avoid_4d':
                     obj = objective_values
                     label_11 = (np.min(np.sign(obj.numpy()), axis=1))
-                    label_11[np.where(label_11 == 0)[0]] = -1  # -1 and 1
+                    label_11[np.where(label_11 <= 0)[0]] = -1  # -1 and 1
                     # label_01 = (np.min((obj.numpy()), axis=1))== 100
                     # label_11 = 2 * label_01 - 1
                     # labels = np.reshape(np.array(label_11), (1, -1))

optimized_dp-master/compute_FRS.py
@@ -38,7 +38,7 @@ my_car = DubinsCar4D(uMin=[-1.1, -0.4], uMax=[1.1, 0.4],
 horizon = 6
 t_step = 0.05
 
-v_init = np.linspace(0.3, 0.6, 31)
+v_init = np.linspace(0.25, 0.3, 6)
 tau = np.arange(start= 0, stop= horizon + t_step, step = t_step)
 
 compMethods = { "TargetSetMode": "minVWithVInit"}

optimized_dp-master/user_definer.py
@@ -26,15 +26,20 @@ import matplotlib
 """
 
 g = Grid(np.array([0, 0, -math.pi, 0.0]), np.array([30, 26.05, math.pi, 0.7+0.1]),
-         4, np.array([600, 521, 31, 61]), [2])
+         4, np.array([600, 521, 31, 31]), [2])
 my_car = DubinsCar4D_new2()
 
 # Load value from my map
-obstacles = np.load("obstacle_grid_4d_ver2.npy")
+obstacle_2d=np.load("/local-scratch/tara/project/WayPtNav-reachability/obstacle_grid_2d.npy")
+# obstacles = np.load("obstacle_grid_4d_ver2.npy")
+obstacles = np.tile(
+    np.expand_dims(obstacle_2d, (-2, -1)),
+    (1,1,31,31)
+)
 
 
 # Velocity constraint - negative level set [0., 0.7]
-velocity_constr = Intersection(Lower_Half_Space(g, 3, 0.7), Upper_Half_Space(g, 3, 0.0))
+velocity_constr = Intersection(Lower_Half_Space(g, 3, 0.6), Upper_Half_Space(g, 3, 0.0))
 
 # Combine it with the original obstacle map
 Initial_value_f = Union(-velocity_constr, obstacles)
@@ -52,5 +57,5 @@ compMethods = { "TargetSetMode": "minVWithV0"}
 
 # Solve the HJ pde
 result = HJSolver(my_car, g, Initial_value_f, tau, compMethods, po2, saveAllTimeSteps=False )
-np.save("V_safe.npy", result)
+np.save("V_safe1.npy", result)
 

training_utils/architecture/resnet50_cnn.py
@@ -26,7 +26,7 @@ def resnet50_cnn(image_size, num_inputs, num_outputs, params, dtype=tf.float32):
                             name='resnet50',
                             include_top=False,
                             pooling=None)
-        resnet50.trainable = False
+        resnet50.trainable = True
 
         # Used to control batch_norm during training vs test time
         is_training = tf.contrib.eager.Variable(True, dtype=tf.bool, name='is_training')
@@ -78,106 +78,6 @@ def resnet50_cnn(image_size, num_inputs, num_outputs, params, dtype=tf.float32):
     model.load_weights(params.resnet50_weights_path, by_name=True)
     # model.load_weights(params.resnet50_weights_path)
     # model.summary()
-    #
-    # # Load the Resnet50 weights
-    # model.load_weights(params.resnet50_weights_path, by_name=True)
-    #
-    # resnet50.build(input_shape=(None, 224, 224, 3))
-    # # print(resnet50.layers[-1])
-    # # resnet50.summary()
-    #
-    # model1 = models.Sequential()
-    #
-    # input1=model1.add(layers.InputLayer(input_shape=(224, 224, 180), name="input_layer1"))
-    # # input2=model1.add(layers.InputLayer(input_shape=(1, 1, 120), name="input_layer2"))
-    # # inputs = tf.keras.layers.Dot(axes=1)([input1, input2])
-    # # model1.add(layers.Dense(3,input_shape=(224, 224, 180)))
-    # model1.add(layers.Conv2D(90, (3, 3),
-    #     strides=(1, 1),
-    #     padding='valid',
-    #      input_shape=(224, 224, 180)))
-    # # model1.add(layers.ZeroPadding2D(padding=(2, 2)))
-    # model1.add(layers.Conv2D(20, (3, 3),
-    #     strides=(1, 1),
-    #     padding='valid'
-    #      ))
-    # model1.add(layers.Conv2D(3, (3, 3),
-    #     strides=(1, 1),
-    #     padding='valid'
-    #     ))
-    #
-    # model1.add(layers.ZeroPadding2D(padding=(2, 2)))
-    # model1.add(layers.ZeroPadding2D(padding=(1, 1)))
-    # # model1.summary()
-    #
-    # for layer in resnet50.layers[:-1]:
-    # # for layer in model.layers[:-15]:
-    # #     print(layer)
-    #     model1.add(layer)
-    #
-    #     # model1.summary()
-    # # print(len(model1.layers))
-    # # print(model1.layers[-1])
-    #
-    # model1.build(input_shape=(224,224,180))
-    #
-    # # model1.summary()
-    # # model1.compile(optimizer='adam')
-    #
-    # # model1.add(layers.MaxPooling2D(2, 2))
-    #
-    #
-    # model3 = models.Sequential()
-    # input2 = model3.add(layers.InputLayer(input_shape=(120,), name="input_layer2"))
-    # model3.add(layers.Dense(60))
-    # model3.add(layers.Dense(20))
-    # model3.add(layers.Dense(2))
-    # model3.build(input_shape=(120,))
-    # # model3.summary()
-    #
-    #
-    # input_image1 = layers.Input(shape=(224, 224, 180), dtype=dtype)
-    # input_flat1 = layers.Input(shape=(120,), dtype=dtype)
 
-    # input_image1 = Input(shape=(224, 224, 180), dtype=dtype)
-    # input_flat1 = Input(shape=(120,), dtype=dtype)
-
-    # x1 = input_image1
-    # x1 = model1.call(x1)
-    # if params.dim_red_conv_2d.use:
-    #     # Convolutional layer
-    #     x1 = layers.Conv2D(
-    #                 filters=params.dim_red_conv_2d.num_outputs,
-    #                 kernel_size=params.dim_red_conv_2d.filter_size,
-    #                 strides=params.dim_red_conv_2d.stride,
-    #                 padding=params.dim_red_conv_2d.padding,
-    #                 activation=params.hidden_layer_activation_func)(x1)
-    #     # Max-pooling layer
-    #     if params.dim_red_conv_2d.use_maxpool:
-    #         x1 = layers.MaxPool2D(pool_size=(params.dim_red_conv_2d.size_maxpool_filters,
-    #                                         params.dim_red_conv_2d.size_maxpool_filters),
-    #                              padding='valid')(x1)
-    #
-    # # # model1.add(model)
-    # # x1 = model.call(x1)
-    # x1 = layers.Flatten()(x1)
-    # input_flat2 = model3.call(input_flat1)
-    # x1 = layers.Concatenate(axis=1)([x1, input_flat2])
-    #
-    # for i in range(params.num_hidden_layers):
-    #     x1 = layers.Dense(params.num_neurons_per_layer, activation=params.hidden_layer_activation_func)(x1)
-    #     if params.use_dropout:
-    #         x1 = layers.Dropout(rate=params.dropout_rate)(x1)
-    #
-    # # Output layer
-    # x1 = layers.Dense(num_outputs, activation=params.output_layer_activation_func)(x1)
-    #
-    # # Generate a Keras model
-    #
-    # model4 = tf.keras.Model(inputs=[input_image1, input_flat1], outputs=x1)
-    # model4.summary()
-    # # model4.compile(
-    # #     optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),
-    # #     loss='mean_absolute_error')
 
     return model, is_training

trajectory/spline/spline_3rd_order.py
@@ -10,7 +10,7 @@ from scipy.optimize import LinearConstraint
 def vehicle_flat_forward(x, u, params={}):
     # Get the parameter values
     # Create a list of arrays to store the flat output and its derivatives
-    zflag = [np.zeros(4), np.zeros(4)]
+    zflag = [np.zeros(3), np.zeros(3)]
     # Flat output is the x, y position of the rear wheels
     zflag[0][0] = x[0]
     zflag[1][0] = x[1]
@@ -23,11 +23,11 @@ def vehicle_flat_forward(x, u, params={}):
     # zflag[2][1] = u[0]
     # zflag[3][1] = u[1]
     # First derivative of the angle
-    thdot = u[0]
-    vdot = u[1]
+    thdot = u[1]
+    vdot = u[0]
     # Second derivatives of the flat output (setting vdot = 0)
-    zflag[0][2] = - vel * thdot * np.sin(theta) + vdot * np.cos(theta)
-    zflag[1][2] = vel * thdot * np.cos(theta) + vdot * np.sin(theta)
+    zflag[0][2] = (- vel * thdot * np.sin(theta)) + (vdot * np.cos(theta))
+    zflag[1][2] = (vel * thdot * np.cos(theta)) + (vdot * np.sin(theta))
     return zflag
 
 
@@ -35,18 +35,32 @@ def vehicle_flat_forward(x, u, params={}):
 def vehicle_flat_reverse(zflag, params={}):
     # Get the parameter values
     # Create a vector to store the state and inputs
-    x = np.zeros(4)
+    X = np.zeros(4)
     u = np.zeros(2)
+
+    x = zflag[0][0]
+    y = zflag[1][0]
+
+    xd = zflag[0][1]
+    yd = zflag[1][1]
+
+    xdd = zflag[0][2]
+    ydd = zflag[1][2]
+
+    theta = np.arctan2(yd, xd+1e-5)
+    v = np.linalg.norm([yd, xd])
+
     # Given the flat variables, solve for the state
-    x[0] = zflag[0][0]  # x position
-    x[1] = zflag[1][0]  # y position
-    x[2] = np.arctan2(zflag[1][1], zflag[0][1])  # tan(theta) = ydot/xdot
-    x[3] = np.linalg.norm([zflag[1][1], zflag[0][1]])
+    X[0] = x  # x position
+    X[1] = y # y position
+    X[2] = theta # tan(theta) = ydot/xdot
+    X[3] = v
     # And next solve for the inputs
-    u[0] = 1 / (1 + (zflag[0][1] / zflag[0][1]) ** 2) * (
-            (zflag[1][2] * zflag[0][1]) - (zflag[0][2] * zflag[1][1])) / (zflag[0][1] ** 2 + 1e-5)
-    u[1] = 0.5 * (1 / x[3]) * (2 * zflag[1][2] * zflag[1][1] + 2 * zflag[0][2] * zflag[0][1])
-    return x, u
+    u[1] = 1 / (1 + (yd / (xd + 1e-5)) ** 2) * (
+            (ydd * xd) - (xdd * yd) / (xd ** 2 + 1e-5)
+    )
+    u[0] = 1.0 / (2*(v + 1e-5)) * ((2 * ydd * yd) + (2 * xdd * xd))
+    return X, u
 
 
 class Spline3rdOrder(Spline):
@@ -61,90 +75,92 @@ class Spline3rdOrder(Spline):
     """
 
 
-    def fit1(self, start_config, goal_config, final_times_n1=None, factors=None):
-
-        u0 = np.asarray([0, 0])
-        uf = np.asarray([0, 0])
-        dt = 0.05
-        t = np.arange(0, 6, dt)
-        Av = np.zeros((3, 6))
-        Av[0, 3] = 1
-        Av[1, 4] = 1
-        Av[2, 5] = 1
-        lbv = [0, -0.4, -1.1]
-        ubv = [0.7, 0.4, 1.1]
-
-        trajectory_constraints = [
-            (LinearConstraint, Av, lbv, ubv)
-        ]
-        vehicle_flat = fs.FlatSystem(forward=vehicle_flat_forward, reverse=vehicle_flat_reverse, inputs=2, states=4)
-        # x0=start_config
-        x0 = start_config.position_heading_speed_nk4()[0].numpy().reshape((4,))
-        # xf=goal_config
-
-        xfs = goal_config.position_heading_speed_nk4().numpy().reshape((-1, 4))
-        xs, ys, headings, speeds, accs, omegas = [], [], [], [], [], []
-        valid_idxs = []
-
-        import tqdm
-        for idx, xf in tqdm.tqdm(enumerate(xfs), total=xfs.shape[0]):
-            try:
-                traj_const = fs.point_to_point(vehicle_flat, t, x0, u0, xf, uf, basis=None,  # fs.PolyFamily(8),
-                                               constraints=trajectory_constraints)
-            except RuntimeError:
-                continue
-            # ,cost=cost_fcn)
-            # Create the trajectory
-            # x, u = traj_const.eval(t)
-            # traj_const = fs.point_to_point(vehicle_flat, t, x0, u0, xf, uf,
-            #                                basis=fs.PolyFamily(8))  # constraints=constraints,
-            # ,cost=cost_fcn)
-            # Create the trajectory
-
-            valid_idxs.append(idx)
-            x, u = traj_const.eval(t)
-            xs.append(x[0, :])
-            ys.append(x[1, :])
-            headings.append(x[2, :])
-            speeds.append(x[3, :])
-            accs.append(u[0, :])
-            omegas.append(u[1, :])
-
-        xs, ys, headings, speeds, accs, omegas \
-            = np.stack(xs), np.stack(ys), np.stack(headings), np.stack(speeds), np.stack(accs), np.stack(omegas)
-
-        xs = xs[:, :, np.newaxis].astype(np.float32)
-        ys = ys[:, :, np.newaxis].astype(np.float32)
-        headings = headings[:, :, np.newaxis].astype(np.float32)
-        omegas = omegas[:, :, np.newaxis].astype(np.float32)
-        accs = accs[:, :, np.newaxis].astype(np.float32)
-        # accs = np.stack([accs[0] for _ in range(21483)])
-        speeds = speeds[:, :, np.newaxis].astype(np.float32)
-
-        # self.spline_trajectory._acceleration_nk1.assign(tf.convert_to_tensor(accs))
-        self.spline_trajectory._position_nk2 = tf.convert_to_tensor(np.concatenate((xs, ys), axis=2))
-        self.spline_trajectory._heading_nk1 = tf.convert_to_tensor(headings)
-        self.spline_trajectory._acceleration_nk1 = tf.convert_to_tensor(accs)
-        self.spline_trajectory._speed_nk1 = tf.convert_to_tensor(speeds)
-        self.spline_trajectory._angular_speed_nk1 = tf.convert_to_tensor(omegas)
-
-        self.spline_trajectory.vars = [self.spline_trajectory._position_nk2, self.spline_trajectory._speed_nk1,
-                                       self.spline_trajectory._acceleration_nk1, self.spline_trajectory._heading_nk1,
-                                       self.spline_trajectory._angular_speed_nk1,
-                                       self.spline_trajectory._angular_acceleration_nk1]
-
-        p = self.params
-        times_nk = tf.tile(tf.linspace(0., p.planning_horizon_s, p.planning_horizon)[None],
-                           [self.n, 1])  # number of waypoints * number of planning horizon. maximum time = 6
-        final_times_n1 = tf.ones((self.n, 1), dtype=tf.float32) * p.planning_horizon_s
-        self.final_times_n1 = final_times_n1
-
-        # Update the batch size as the same spline object
-        # can be used with multiple start/ goal configurations
-        self.n = start_config.n
-        self.valid_horizons_n1 = tf.ceil(self.final_times_n1 / self.dt)
-
-        return valid_idxs
+    # def fit1(self, start_config, goal_config, final_times_n1=None, factors=None):
+    #
+    #     u0 = np.asarray([0, 0])
+    #     uf = np.asarray([0, 0])
+    #     dt = 0.05
+    #     t = np.arange(0, 6, dt)
+    #     Av = np.zeros((3, 6))
+    #     Av[0, 3] = 1
+    #     Av[1, 4] = 1
+    #     Av[2, 5] = 1
+    #     lbv = [0, -0.4, -1.1]
+    #     ubv = [0.7, 0.4, 1.1]
+    #
+    #     trajectory_constraints = [
+    #         (LinearConstraint, Av, lbv, ubv)
+    #     ]
+    #     vehicle_flat = fs.FlatSystem(forward=vehicle_flat_forward, reverse=vehicle_flat_reverse, inputs=2, states=4)
+    #     # x0=start_config
+    #     x0 = start_config.position_heading_speed_nk4()[0].numpy().reshape((4,))
+    #     # xf=goal_config
+    #
+    #     xfs = goal_config.position_heading_speed_nk4().numpy().reshape((-1, 4))
+    #     xs, ys, headings, speeds, accs, omegas = [], [], [], [], [], []
+    #     valid_idxs = []
+    #
+    #     import tqdm
+    #     for idx, xf in tqdm.tqdm(enumerate(xfs), total=xfs.shape[0]):
+    #         try:
+    #             traj_const = fs.point_to_point(
+    #                 vehicle_flat, t, x0, u0, xf, uf, basis=None,  # fs.PolyFamily(8),
+    #                 # constraints=trajectory_constraints
+    #             )
+    #         except RuntimeError:
+    #             continue
+    #         # ,cost=cost_fcn)
+    #         # Create the trajectory
+    #         # x, u = traj_const.eval(t)
+    #         # traj_const = fs.point_to_point(vehicle_flat, t, x0, u0, xf, uf,
+    #         #                                basis=fs.PolyFamily(8))  # constraints=constraints,
+    #         # ,cost=cost_fcn)
+    #         # Create the trajectory
+    #
+    #         valid_idxs.append(idx)
+    #         x, u = traj_const.eval(t)
+    #         xs.append(x[0, :])
+    #         ys.append(x[1, :])
+    #         headings.append(x[2, :])
+    #         speeds.append(x[3, :])
+    #         accs.append(u[0, :])
+    #         omegas.append(u[1, :])
+    #
+    #     xs, ys, headings, speeds, accs, omegas \
+    #         = np.stack(xs), np.stack(ys), np.stack(headings), np.stack(speeds), np.stack(accs), np.stack(omegas)
+    #
+    #     xs = xs[:, :, np.newaxis].astype(np.float32)
+    #     ys = ys[:, :, np.newaxis].astype(np.float32)
+    #     headings = headings[:, :, np.newaxis].astype(np.float32)
+    #     omegas = omegas[:, :, np.newaxis].astype(np.float32)
+    #     accs = accs[:, :, np.newaxis].astype(np.float32)
+    #     # accs = np.stack([accs[0] for _ in range(21483)])
+    #     speeds = speeds[:, :, np.newaxis].astype(np.float32)
+    #
+    #     # self.spline_trajectory._acceleration_nk1.assign(tf.convert_to_tensor(accs))
+    #     self.spline_trajectory._position_nk2 = tf.convert_to_tensor(np.concatenate((xs, ys), axis=2))
+    #     self.spline_trajectory._heading_nk1 = tf.convert_to_tensor(headings)
+    #     self.spline_trajectory._acceleration_nk1 = tf.convert_to_tensor(accs)
+    #     self.spline_trajectory._speed_nk1 = tf.convert_to_tensor(speeds)
+    #     self.spline_trajectory._angular_speed_nk1 = tf.convert_to_tensor(omegas)
+    #
+    #     self.spline_trajectory.vars = [self.spline_trajectory._position_nk2, self.spline_trajectory._speed_nk1,
+    #                                    self.spline_trajectory._acceleration_nk1, self.spline_trajectory._heading_nk1,
+    #                                    self.spline_trajectory._angular_speed_nk1,
+    #                                    self.spline_trajectory._angular_acceleration_nk1]
+    #
+    #     p = self.params
+    #     times_nk = tf.tile(tf.linspace(0., p.planning_horizon_s, p.planning_horizon)[None],
+    #                        [self.n, 1])  # number of waypoints * number of planning horizon. maximum time = 6
+    #     final_times_n1 = tf.ones((self.n, 1), dtype=tf.float32) * p.planning_horizon_s
+    #     self.final_times_n1 = final_times_n1
+    #
+    #     # Update the batch size as the same spline object
+    #     # can be used with multiple start/ goal configurations
+    #     self.n = start_config.n
+    #     self.valid_horizons_n1 = tf.ceil(self.final_times_n1 / self.dt)
+    #
+    #     return valid_idxs
 
 
     def fit(self, start_config, goal_config, final_times_n1=None, factors=None):  # Get the coefficients of Spline based on start and goal