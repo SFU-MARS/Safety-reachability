60e308c327dae328ac8e822f667c031aba397019
buinm <b.minh96@gmail.com>
2023-09-11 18:03:21 -0700

    Adding waypoints onto the image

--- interact_demo/test_pyrealsense.py
+++ interact_demo/test_pyrealsense.py
@@ -38,36 +38,40 @@ else:
 # Start streaming
 pipeline.start(config)
 
+# World-frame x-y waypoints
+# TODO: This should be streamed directly from another thread that computes the SVM boundary
+#wpts_list = [[-2., 3.], [-5., 2.]]
+
 try:
     while True:
 
         # Wait for a coherent pair of frames: depth and color
         frames = pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
+        # depth_frame = frames.get_depth_frame()
         color_frame = frames.get_color_frame()
-        if not depth_frame or not color_frame:
+        if not color_frame:
             continue
 
         # Convert images to numpy arrays
-        depth_image = np.asanyarray(depth_frame.get_data())
-        color_image = np.asanyarray(color_frame.get_data())
+        #depth_image = np.asanyarray(depth_frame.get_data())
+        rgb_image = np.asanyarray(color_frame.get_data())
 
-        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+        """
+        List of things to do:
+            - Convert the wpts_list into image frame
+            - Plot the wpts onto the image
+            - Detect clicking events for the plotted waypoints in the image
+        """
 
-        depth_colormap_dim = depth_colormap.shape
-        color_colormap_dim = color_image.shape
+        wpts_list =  [(20, 200), (200, 200)]
 
-        # If depth and color resolutions are different, resize color image to match depth image for display
-        if depth_colormap_dim != color_colormap_dim:
-            resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
-            images = np.hstack((resized_color_image, depth_colormap))
-        else:
-            images = np.hstack((color_image, depth_colormap))
+        # For now, let's just plot wpts onto the image
+        for wpts in wpts_list:
+            rgb_image = cv2.circle(rgb_image, wpts, radius=3, color=(0, 0, 255), thickness=1)
 
         # Show images
         cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('RealSense', images)
+        cv2.imshow('RealSense', rgb_image)
         cv2.waitKey(1)
 
 finally:
